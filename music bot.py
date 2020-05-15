c8aa95e435a304f129beacb84dabef9b9402d56c
yung-lin94 <62969352+yung-lin94@users.noreply.github.com>
2020-05-15 15:23:06 +0800

    ping inbox

--- music bot.py
+++ music bot.py
@@ -9,15 +9,8 @@ async def on_ready():
     print(">> Bot is online <<")
 
 
-@bot.event
-async def on_member_join(member):
-    channel = bot.get_channel(710724588379832331)
-    await channel.send(f'{member}歡迎家入♡')
-
-
-@bot.event
-async def on_member_remove(member):
-    channel = bot.get_channel(710724588379832331)
-    await channel.send(f'{member}留下來嘛!')
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'{round(bot.latency*1000)}(ms)')
 
 bot.run('NzEwNTE5NjEzNDYwMzE2MjQw.Xr4eXg.nHTsHEs1mKGPTFGlZDUtHiHilAw')
