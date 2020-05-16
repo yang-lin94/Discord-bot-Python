dec9b4d96ff6d6008871c02eefe392787d714114
yung-lin94 <62969352+yung-lin94@users.noreply.github.com>
2020-05-16 20:03:25 +0800

    say mod

--- cmds/main.py
+++ cmds/main.py
@@ -9,6 +9,11 @@ class Main(Cog_Extension):
     async def ping(self, ctx):
         await ctx.send(f'{round(self.bot.latency*1000)}(ms)')
 
+    @commands.command()
+    async def sy(self, ctx, msg):
+        await ctx.message.delete()
+        await ctx.send(msg)
+
 
 def setup(bot):
     bot.add_cog(Main(bot))
