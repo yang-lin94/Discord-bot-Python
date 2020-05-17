83de364c8a8646171dc61722d42cc0af384037fb
yung-lin94 <62969352+yung-lin94@users.noreply.github.com>
2020-05-17 19:13:21 +0800

    task mode

--- cmds/task.py
+++ cmds/task.py
@@ -10,6 +10,10 @@ class task(Cog_Extension):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+    async def interval():
+        await self.bot.wait_until_realy()
+        while not self.bot.is_closed():
+
 
 def setup(bot):
     bot.add_cog(Task(bot))
